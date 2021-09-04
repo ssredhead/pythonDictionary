b5203e37fafd65cf3a68325120b5e4eeec2e36e0
Sophia Sommers <soommers.sophia@gmail.com>
2021-09-04 19:44:32 -0400

    euclideanDistance function

---
+++ euclideanDistance.py
@@ -0,0 +1,10 @@
+def euclidean_distance(pt1, pt2):
+  distance = 0
+  for i in range(len(pt1)):
+    distance += (pt1[i] - pt2[i]) ** 2 
+  return distance ** 0.5
+
+
+print(euclidean_distance([1,2], [4,0]))
+print(euclidean_distance([5,4,3], [1,7,9]))
+#The lists must be the same length to compare euclidean distances
