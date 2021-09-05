6c01cb83e34cfeabd51250b72b97fec1ba6d3246
Sophia Sommers <soommers.sophia@gmail.com>
2021-09-05 14:45:15 -0400

    made formula more general

--- MLDistanceFormula.py
+++ MLDistanceFormula.py
@@ -1,12 +1,12 @@
-star_wars = [125, 1977]
-raiders = [115, 1981]
-mean_girls = [97, 2004]
+star_wars = [125, 1977, 11000000]
+raiders = [115, 1981, 18000000]
+mean_girls = [97, 2004, 17000000]
 
 def distance(movie1, movie2):
-  distance = ((movie1[0] - movie2[0]) ** 2 + (movie1[1] - movie2[1]) ** 2) ** 0.5
-  return distance
+  distance = 0
+  for i in range(len(movie1)):
+    distance += (movie1[i] - movie2[i]) ** 2
+  return distance ** 0.5
 
-
-print(distance(star_wars, raiders)) # 10.77
-print(distance(star_wars, mean_girls)) # 38.89
-#Star Wars is more similar to Raiders of the Lost Arc
+print(distance(star_wars, raiders)) # farther
+print(distance(star_wars, mean_girls)) # closer
