6840df0cd7fbbc8fa5b76c384d97cd6240451729
Sophia Sommers <soommers.sophia@gmail.com>
2021-09-17 20:24:29 -0400

    using sklearn kmean cluster

---
+++ sklearnKMean.py
@@ -0,0 +1,19 @@
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.cluster import KMeans
+
+# From sklearn.cluster, import KMeans class
+
+iris = datasets.load_iris()
+
+samples = iris.data
+
+# Use KMeans() to create a model that finds 3 clusters
+model = KMeans(n_clusters = 3)
+# Use .fit() to fit the model to samples
+model.fit(samples)
+# Use .predict() to determine the labels of samples 
+labels = model.predict(samples)
+# Print the labels
+print(labels)
