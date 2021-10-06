1c8a1c209e939221cad3329bbb435d72dbe6045b
Sophia Sommers <soommers.sophia@gmail.com>
2021-10-05 20:35:29 -0400

    updating to use best move

--- minmax/minmaxfunction.py
+++ minmax/minmaxfunction.py
@@ -33,7 +33,8 @@ o_winning = [
 def minimax(input_board, is_maximizing):
   # Base case - the game is over, so we return the value of the board
   if game_is_over(input_board):
-    return evaluate_board(input_board)
+    return [evaluate_board(input_board), ""]
+  best_move = ""
   if is_maximizing == True:
     best_value = -float("Inf")
     symbol = "X"
@@ -43,12 +44,16 @@ def minimax(input_board, is_maximizing):
   for move in available_moves(input_board):
     new_board = deepcopy(input_board)
     select_space(new_board, move, symbol)
-    hypothetical_value = minimax(new_board, not is_maximizing)
+    hypothetical_value = minimax(new_board, not is_maximizing)[0]
     if is_maximizing == True and hypothetical_value > best_value:
       best_value = hypothetical_value
+      best_move = move
     if is_maximizing == False and hypothetical_value < best_value:
       best_value = hypothetical_value
-  return best_value
-  
+      best_move = move
+  return [best_value, best_move]
+
+# This should return [1, 7]. This means that "X" should be able to win the game if they select move 7.
 print(minimax(x_winning, True))
+# This should return [-1, 4]. This means that no matter what "X" does, "O" will win. "X" might as well select move 4.
 print(minimax(o_winning, True))
