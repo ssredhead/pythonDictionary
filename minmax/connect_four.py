20ef9c9ccf7e64e4973b041313baca35cdcfa8bf
Sophia Sommers <soommers.sophia@gmail.com>
2021-10-17 18:21:32 -0400

    adding streaks

--- minmax/connect_four.py
+++ minmax/connect_four.py
@@ -4,8 +4,18 @@ def random_eval():
   return random.randint(-100, 100)
 
 def my_evaluate_board(board):
+  x_two_streak = 0
+  o_two_streak = 0
   if has_won(board, "X"):
     return float("Inf")
+
+  for col in range(len(board)):
+    for row in range(len(board[0])):
+      if board[col][row] == "X" and board[col+1][row] == "X": 
+        x_two_streak += 1
+      if board[col][row] == "O" and board[col+1][row] == "O": 
+        o_two_streak += 1
+  
 def two_ai_game():
     my_board = make_board()
     while not game_is_over(my_board):
