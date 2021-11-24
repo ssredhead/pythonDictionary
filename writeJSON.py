1ed5d30fcd4e0f741a1ddfdbe92686190615fae8
Sophia Sommers <soommers.sophia@gmail.com>
2021-11-23 20:34:02 -0500

    adding comments

--- writeJSON.py
+++ writeJSON.py
@@ -1,5 +1,9 @@
 import json 
 
+#the json library does not need to be preinstalled
+
+#key, value pair will turn into json field name and data
+
 data_payload = [
   {'interesting message': 'What is JSON? A web application\'s little pile of secrets.',
    'follow up': 'But enough talk!'}
