ffc669eb0349e5d943971cddd45233b4a3efba8f
Sophia Sommers <soommers.sophia@gmail.com>
2021-12-05 18:00:06 -0500

    continuation

--- classProject.py
+++ classProject.py
@@ -9,3 +9,7 @@ class Menu:
 brunch = Menu("brunch", {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
 }, 11, 16)
+
+early_bird = Menu("early bird", {
+  'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+}, 15, 17)
