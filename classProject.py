f7642bba45ab7af35bb730ca82ac265a70652c83
Sophia Sommers <soommers.sophia@gmail.com>
2021-12-06 19:26:37 -0500

    more class stuff

--- classProject.py
+++ classProject.py
@@ -13,3 +13,7 @@ brunch = Menu("brunch", {
 early_bird = Menu("early bird", {
   'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
 }, 15, 17)
+
+dinner = Menu("dinner", {'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,}, 17, 23)
+
+kids = Menu("kids", {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}, 11, 21)
