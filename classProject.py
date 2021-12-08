d9a7fb47694615b0f612c9637f6481092de5bec9
Sophia Sommers <soommers.sophia@gmail.com>
2021-12-07 19:27:09 -0500

    string representation method

--- classProject.py
+++ classProject.py
@@ -5,6 +5,9 @@ class Menu:
     self.start_time = start_time
     self.end_time = end_time
 
+    def __repr__(self):
+    return self.name
+
 
 brunch = Menu("brunch", {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
