6da573bb43e0647755d05a5fdf99b38861fff991
Sophia Sommers <soommers.sophia@gmail.com>
2021-12-08 19:38:31 -0500

    new function

--- classProject.py
+++ classProject.py
@@ -6,8 +6,13 @@ class Menu:
     self.end_time = end_time
 
     def __repr__(self):
-    return self.name
+      return self.name
 
+  def calculate_bill(self, purchased_items):
+    total = 0
+    for item in purchased_items:
+      total += self.items[item]
+    return total
 
 brunch = Menu("brunch", {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
