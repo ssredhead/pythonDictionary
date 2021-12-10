ca99f8148f5a85412d7c116239e1d6544b15c3f3
Sophia Sommers <soommers.sophia@gmail.com>
2021-12-09 21:16:21 -0500

    new class

--- classProject.py
+++ classProject.py
@@ -14,6 +14,11 @@ class Menu:
       total += self.items[item]
     return total
 
+class Franchise:
+  def __init__(self, address, menus):
+    self.address = address
+    self.menus = menus
+
 brunch = Menu("brunch", {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
 }, 11, 16)
