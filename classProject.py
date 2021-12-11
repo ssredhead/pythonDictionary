3d074b916b791024afb50bf2de4e1e4512492212
Sophia Sommers <soommers.sophia@gmail.com>
2021-12-10 19:10:18 -0500

    final

--- classProject.py
+++ classProject.py
@@ -5,8 +5,8 @@ class Menu:
     self.start_time = start_time
     self.end_time = end_time
 
-    def __repr__(self):
-      return self.name
+  def __repr__(self):
+    return self.name
 
   def calculate_bill(self, purchased_items):
     total = 0
@@ -19,6 +19,21 @@ class Franchise:
     self.address = address
     self.menus = menus
 
+  def __repr__(self):
+    return self.address
+
+  def available_menus(self, time):
+    return_menus = []
+    for menu in self.menus:
+      if time >= menu.start_time and time <= menu.end_time:
+        return_menus.append(menu)
+    return return_menus
+
+class Business:
+  def __init__(self, name, franchises):
+    self.name = name
+    self.franchises = franchises
+
 brunch = Menu("brunch", {
   'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
 }, 11, 16)
@@ -30,3 +45,25 @@ early_bird = Menu("early bird", {
 dinner = Menu("dinner", {'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,}, 17, 23)
 
 kids = Menu("kids", {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}, 11, 21)
+
+flagship_store = Franchise("1232 West End Road", [brunch, kids, early_bird, dinner])
+
+new_installment = Franchise("12 East Mulberry Street", [brunch, kids, early_bird, dinner])
+
+business_1 = Business("Basta Fazoolin' with my Heart", [flagship_store, new_installment])
+
+arepa_menu = Menu("Take a' Arepa", {
+  'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50
+}, 10, 20)
+
+arepas_place = Franchise("189 Fitzgerald Avenue", [arepa_menu])
+
+final_business = Business("Take a' Arepa", [arepas_place])
+
+
+# print(brunch)
+# print(brunch.calculate_bill(['pancakes', 'home fries', 'coffee']))
+
+# print(early_bird.calculate_bill(['salumeria plate','mushroom ravioli (vegan)']))
+
+# print(new_installment.available_menus(17))
